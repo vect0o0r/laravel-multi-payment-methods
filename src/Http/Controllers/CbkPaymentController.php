e878878652c7a8aae587b6fa414746bf8bdce613
Vector <mo.khaled.yousef@gmail.com>
2022-12-16 21:49:05 +0200

    add cbk payment

--- src/Http/Controllers/CbkPaymentController.php
+++ src/Http/Controllers/CbkPaymentController.php
@@ -1,12 +1,16 @@
 <?php
+
 namespace Vector\LaravelMultiPaymentMethods\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class CbkPaymentController extends Controller
 {
 
-    public function paymentView()
+    public function paymentView(Request $request)
     {
-        dd('hhhhhhhh');
+        return View::make('payment::cbkPaymentView.blade', ['params' => $request->all()]);
     }
 
 }
