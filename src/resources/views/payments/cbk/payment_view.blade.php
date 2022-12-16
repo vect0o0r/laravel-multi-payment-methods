eb2be2405abde69f88c0f5e244025ca6c5fe9a39
Vector <mo.khaled.yousef@gmail.com>
2022-12-16 22:36:20 +0200

    add cbk payment

--- src/resources/views/payments/cbk/payment_view.blade.php
+++ src/resources/views/payments/cbk/payment_view.blade.php
@@ -1,18 +1,21 @@
 @php
     $url = $params['url']."/ePay/pg/epay?_v=" .$params['tij_MerchAuthKeyApi'];
-
 @endphp
 <form id='pgForm' method='post' action="{{$url}}" enctype='application/x-www-form-urlencoded'>
     @php
-        foreach ($params as $key => $value) {
-    echo "<input type='hidden' name='$key' value='$value'>";
-      }
+        $formKeys = Arr::except($params,['url']);
+            foreach ($formKeys as $key => $value) {
+        echo "<input type='hidden' name='$key' value='$value'>";
+          }
     @endphp
 
 </form>
 
-<div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);text-align:center">Redirecting to PG ...
-    <br> <b> DO NOT REFRESH</b></div>
+<div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);text-align:center">
+    Redirecting to PG ...
+    <br>
+    <b> DO NOT REFRESH</b>
+</div>
 <script type="text/javascript">
     document.getElementById('pgForm').submit();
 </script>
